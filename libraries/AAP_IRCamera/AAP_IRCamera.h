834b125720a258e988c7826fdf568afe41029c58
Nick Yeung <nic.yeung@gmail.com>
2012-04-17 14:51:01 -0700

    Update libraries/AAP_IRCamera/AAP_IRCamera.h

--- libraries/AAP_IRCamera/AAP_IRCamera.h
+++ libraries/AAP_IRCamera/AAP_IRCamera.h
@@ -1,5 +1,7 @@
 #ifndef AAP_IRCamera_h
 #define AAP_IRCamera_h
+#define byte uint8_t
+#include <inttypes.h>
 
 #include "../AP_Math/AP_Math.h"
 
@@ -9,9 +11,15 @@ class AAP_IRCamera
 	AAP_IRCamera();
 
 	//Assumes sources is an array of size 4
-	//The IR source is invalid if sources[i].x and sources[i].y are equal to 1023? @Nick: revise this comment
+	//The IR source is invalid if sources[i].x and sources[i].y are equal to 1023
+	void init();
 	void getRawData(Vector2i sources[]);
 
 	Vector3f getPosition();
+  private:
+  	int _IRsensorAddress;
+	int _slaveAddress;
+	byte _data_buf[16];
+	void Write_2bytes(byte d1, byte d2);
 };
 #endif
